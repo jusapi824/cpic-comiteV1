f9051aeece1c9e98ed7ff36f1c54a82ccf9c01d9
Julian Salazar <jsalazar@sena.edu.co>
2024-12-26 18:42:02 -0500

    Cambios de configuración 6

--- op_login.php
+++ op_login.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $response = $_POST['g-recaptcha-response'];
     
     // Verificar que la respuesta de reCAPTCHA no esté vacía
-    if (false || empty($response)) {
+    if (false && empty($response)) {
         header('Location: login.php?error=3'); // Redirigir si no se completó el reCAPTCHA
         exit;
     }
