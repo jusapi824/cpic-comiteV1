9366aec859a431601a516b34bdaabf8fc89e06a2
Julian Salazar <jsalazar@sena.edu.co>
2025-01-22 18:14:03 -0500

    Ajustes configuración conexión 7

--- instructor/crear.php
+++ instructor/crear.php
@@ -21,9 +21,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($documento  && $nombres && $apellidos && $celular &&  $correo_electronico && $estado) {
         // Preparar la consulta para insertar los datos en la base de datos
         $stmt = $conn->prepare("INSERT INTO instructor ( documento,nombres, apellidos, celular,  correo_electronico,estado, fecha_creacion, fecha_actualizacion) VALUES (?, ?, ?, ?, ?, ?,?,?)");
-        
+        $fechaCreacion = 0;
+        $fechaActualizacion = 0;
         // Vincular los parámetros correctamente (todos como strings, excepto celular que puede ser numérico)
-        $stmt->bind_param("ssssssss", $documento, $nombres, $apellidos, $celular, $correo_electronico, $estado,0,0);
+        $stmt->bind_param("ssssssss", $documento, $nombres, $apellidos, $celular, $correo_electronico, $estado,$fechaCreacion,$fechaActualizacion);
 
         // Ejecutar la consulta e informar el resultado
         if ($stmt->execute()) {
